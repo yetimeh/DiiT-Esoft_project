e21d8236bed5b7fe9ed6a970f43abb0c25b03287
yetimeh <senukabandara2006@gmail.com>
2024-03-02 21:10:30 +0530

    Add gitignore

--- SaleSync/SaleSync/obj/Debug/net8.0-windows/SaleSync.AssemblyInfo.cs
+++ SaleSync/SaleSync/obj/Debug/net8.0-windows/SaleSync.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SaleSync")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d72ee6b3d259eaa3876b54d50a206f89e93e7cac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SaleSync")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SaleSync")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
